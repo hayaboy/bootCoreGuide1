1d7953293d38d8cf1a3cc826a26e2a2cea9c04bf
kds <hayaboy@korea.ac.kr>
2024-03-20 23:16:04 +0900

    24_3_20_bootAWSWebService

---
+++ src/main/java/com/example/bootcoreguide1/BootCoreGuide1Application.java
@@ -0,0 +1,13 @@
+package com.example.bootcoreguide1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCoreGuide1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCoreGuide1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootcoreguide1/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.bootcoreguide1;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+@WebMvcTest
+public class HelloControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    public void hello() throws Exception{
+        String hello="hello";
+        mockMvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+    
+    
+}
